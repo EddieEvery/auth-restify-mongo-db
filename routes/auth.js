00565ca909164b0e3d02a41ca2faf190a6c8de80
every the great <nayiturikieddie@gmail.com>
2022-10-26 22:58:21 -0700

    first commit

--- routes/auth.js
+++ routes/auth.js
@@ -4,11 +4,14 @@ const httpErrors = require('restify-errors/lib/httpErrors');
 const User = mongoose.model('User');
 const auth = require('../auth');
 
+
+
 exports.authenticate = (email, password) =>{
     return new Promise(async(resolve, reject)=>{
   try{
     //Get user by email
     const user = await User.findOne({email});
+
 
     //Match Password
     bcrypt.compare(password,user.password,(err,isMatch)=>{
@@ -18,10 +21,12 @@ exports.authenticate = (email, password) =>{
             reject('Authentication Failed');
         }
     });
+
   }catch(err){
     //email not found
     reject('Authentication Failed');
   }
+
 
     });
 
